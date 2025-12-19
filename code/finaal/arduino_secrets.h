4c2991e59f8d0307321af936a68963b45c18a7cb
Niels1310 <niels.dewaele@student.hogent.be>
2025-12-19 12:04:54 +0100

    Update arduino_secrets.h

--- code/finaal/arduino_secrets.h
+++ code/finaal/arduino_secrets.h
@@ -1,3 +1,3 @@
 #define SECRET_DEVICE_KEY "PqFVzuvZ6ooX0o#@peRJKKf#!"
-#define SECRET_OPTIONAL_PASS "cyoqi18957"
-#define SECRET_SSID "draadloos2"
+#define SECRET_OPTIONAL_PASS "wachtwoord wifi"
+#define SECRET_SSID "wifi naam"
